538a33f05a24d52f8c5e2cb3f86177ec66b520ae
Lucia Lisdero <lulisdero20@gmail.com>
2023-05-11 15:35:25 -0300

    package

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './components/LandingPage/LandingPage';
 import Home from './components/Home/Home';
 import PokemonCreate from './components/PokemonCreate/PokemonCreate';
 import Detail from './components/Detail/Detail';
+import axios from axios;
 
 function App() {
   return (
